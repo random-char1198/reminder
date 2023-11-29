55fca708fd274600b49301309f5ff1d956d5ddbb
P A I <silence1198@outlook.com>
2023-11-29 13:22:58 -0500

    update select date
    <- and -> can now change adte

--- hello WatchKit Extension/Views/SelectiReminderView.swift
+++ hello WatchKit Extension/Views/SelectiReminderView.swift
@@ -8,27 +8,37 @@
 import SwiftUI
 
 struct SelectiReminderView: View {
-    
+    @State private var dateStr: String = ""
     enum ButtonState {
         case normal, green, red
     }
     
+    
     @State private var buttonStates: [Int: ButtonState] = [:]
-    let currentDate:Date = Date()
+    @State private var currentDate:Date = Date()
     
     var habits:HabitsDataBinding
     
     var body: some View {
         ScrollView {
-//            Text(habits.display())
-            Text(formatDate())
-                .frame(maxWidth: .infinity, alignment: .leading)
+            //            Text(habits.display())
+            Text(dateStr)
+                .onAppear(perform: {
+                    
+                    let formatter = DateFormatter()
+                    formatter.dateStyle = .medium
+                    formatter.timeStyle = .none
+                    
+                    self.dateStr = formatter.string(from: currentDate)
+                    
+                })
+            .frame(maxWidth: .infinity, alignment: .leading)
             VStack {
                 ForEach(habits.arrHabits.indices, id: \.self) { index in
                     if habits.arrHabits[index] == 1 {
-//                        Button("\(habits.retrieveItem(id: index))") {
-//                            toggleButtonState(index: index)
-//                        }
+                        //                        Button("\(habits.retrieveItem(id: index))") {
+                        //                            toggleButtonState(index: index)
+                        //                        }
                         let name = habits.retrieveItem(id: index)
                         CustomizeButton(title: name, hapticName: .success, color: nil, action: {
                             toggleButtonState(index: index)
@@ -40,9 +50,11 @@ struct SelectiReminderView: View {
             }
             HStack(content: {
                 Button("", systemImage: "arrowshape.left.fill", action: {
+                    formatDate(dateSelection: false)
                     //switching date
                 })
                 Button("",systemImage: "arrowshape.right.fill",action: {
+                    formatDate(dateSelection: true)
                     
                 })
             })
@@ -52,36 +64,56 @@ struct SelectiReminderView: View {
     private func toggleButtonState(index: Int) {
         let currentState = buttonStates[index] ?? .normal
         switch currentState {
-            case .normal:
-                buttonStates[index] = .green
-            case .green:
-                buttonStates[index] = .red
-            case .red:
-                buttonStates[index] = .green
+        case .normal:
+            buttonStates[index] = .green
+        case .green:
+            buttonStates[index] = .red
+        case .red:
+            buttonStates[index] = .green
         }
     }
     
     private func backgroundColor(for index: Int) -> Color {
         let state = buttonStates[index] ?? .normal
         switch state {
-            case .normal:
-                return Color(red: 34 / 255, green: 34 / 255, blue: 35 / 255)
-            case .green:
-                return .green
-            case .red:
-                return .red
+        case .normal:
+            return Color(red: 34 / 255, green: 34 / 255, blue: 35 / 255)
+        case .green:
+            return .green
+        case .red:
+            return .red
         }
     }
-    private func formatDate() -> String{
+    private func formatDate(dateSelection:Bool) -> Void{
         let formatter = DateFormatter()
         formatter.dateStyle = .medium
         formatter.timeStyle = .none
-
-        let dateString = formatter.string(from: currentDate)
         
-        return dateString
+        
+        // true is tomorrow, false is yesterday
+        if dateSelection{
+            if let tomorrow = Calendar.current.date(byAdding:.day,value: 1, to: self.currentDate){
+                self.currentDate = tomorrow
+
+                self.dateStr = formatter.string(from: tomorrow)
+
+            }
+        }
+        else{
+            if let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: self.currentDate){
+                self.currentDate = yesterday
+
+                self.dateStr = formatter.string(from: yesterday)
+            }
+        }
+        
     }
 }
+
+//Button("Show Tomorrow") {
+//    let tomorrow = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
+//    self.dateStr = formatDate(date: tomorrow)
+//}
 
 //#Preview {
 //    SelectiReminderView(habits: [1,2,3])
